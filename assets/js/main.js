e84583e2c2eb57ad99960b4ca8118423002fa523
Arup-SMB <arupmoulick1@gmail.com>
2024-07-20 19:39:55 +0530

    90% complited ths

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
 $(".hero-area").owlCarousel({
     items: 1,
     loop: true,
-    autoplay: false,
+    autoplay: true,
     dots: true,
     autoplayTimeout: 3000,
     smartSpeed: 750,
+});
+$(".top-destination").owlCarousel({
+    items: 5,
+    loop: true,
+    autoplay: true,
+    dots: true,    
+    autoplayTimeout: 3000,
+    smartSpeed: 750,
+});
+
+// Banner Btns
+var slider = $(".owl-carousel");
+
+$(".carousel-prev").click(function() {
+    slider.trigger("prev.owl.carousel")
+});
+
+$(".carousel-next").click(function() {
+    slider.trigger("next.owl.carousel")
 });
